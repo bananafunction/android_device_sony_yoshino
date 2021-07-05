ecfea130188a620a1b93b6e01521f54c91ea0106
bananafunction <terathree2@web.de>
2021-07-05 19:27:56 +0200

    yoshino: build kernel with CLANG
    Signed-off-by: bananafunction <terathree2@web.de>

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -40,7 +40,7 @@ ENABLE_CPUSETS := true
 ### KERNEL
 TARGET_KERNEL_VERSION := 4.4
 TARGET_KERNEL_SOURCE  := kernel/sony/msm8998
-TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # Taken from unpacked stock boot.img / README_Xperia in Kernel source
 BOARD_KERNEL_CMDLINE += user_debug=31
